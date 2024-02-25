dffe06dc4586211a2cc9b6f872672c3c9f33a868
Mahendhar <7093741662mahiv@gmail.com>
2024-02-25 17:09:21 +0530

    la_2

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -8,6 +8,8 @@ import InfoCard, { CTA, RoundIcon } from '../components/cards/InfoCard'
 function Cards() {
   return (
     <>
+      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
+
 
       <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
 
@@ -21,7 +23,6 @@ function Cards() {
         </CardBody>
       </Card>
       
-      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
      
 
       <CTA />
