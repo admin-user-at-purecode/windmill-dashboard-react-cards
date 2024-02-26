3481762cd1d0ed87b318317e958eb891250730cf
Mahendhar <7093741662mahiv@gmail.com>
2024-02-26 18:18:31 +0530

    cd_4

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -11,7 +11,6 @@ function Cards() {
       <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
      
 
-      <CTA />
 
       <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
 
