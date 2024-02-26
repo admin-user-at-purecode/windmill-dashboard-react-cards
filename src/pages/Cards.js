cddf391bd1ce0b4a16628fbac1c417dbb15e5410
Mahendhar <7093741662mahiv@gmail.com>
2024-02-26 15:08:55 +0530

    la_11

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -11,7 +11,6 @@ function Cards() {
       <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
      
 
-      <CTA />
 
       <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
 
@@ -92,6 +91,33 @@ function Cards() {
           </CardBody>
         </Card>
       </div>
+      <div className="grid gap-6 mb-8 md:grid-cols-2">
+        <Card className="cardWithHeading">
+          <CardBody>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Revenue</p>
+            <p className="text-gray-600 dark:text-gray-400">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
+              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
+              ratione! Ratione, nihil dolorum.
+            </p>
+          </CardBody>
+        </Card>
+
+        <Card colored className="text-white bg-purple-600 cardWithHeading">
+          <CardBody>
+            <p className="mb-4 font-semibold">Colored card</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
+              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
+              ratione! Ratione, nihil dolorum.
+            </p>
+          </CardBody>
+        </Card>
+      </div>
+
+      <CTA />
+
+
     </>
   )
 }
