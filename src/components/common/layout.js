a076e17ab3a19d398267e3d9d1e3f7856add9911
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-08 13:01:43 +0530

    cd_5

--- src/components/common/layout.js
+++ src/components/common/layout.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Windmill } from '@windmill/react-ui';
-import Sidebar from '../sidebar/sidebar';
 import Header from '../header/header';
 
 const Layout = ({ children }) => {
   return (
     <Windmill usePreferences>
-      <div className="flex h-screen">
-        <Sidebar />
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col">
           <Header />
           <div className="flex-1 p-4 dark:bg-gray-800">
             {children}
           </div>
         </div>
-      </div>
     </Windmill>
   );
 };
