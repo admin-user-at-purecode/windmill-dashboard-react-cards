b193c3ed7eb85ffbb5834ab5e9ae7443bb62b2a5
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-08 17:37:56 +0530

    la_22

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,32 +1,106 @@
-import React from 'react'
-import { Card, CardBody } from '@windmill/react-ui'
-import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from '../assets/icons'
+import React from "react";
+import {
+  Card,
+  CardBody,
+  Input,
+  Label,
+  Select,
+  Textarea,
+} from "@windmill/react-ui";
+import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from "../assets/icons";
 
-import InfoCard, { CTA, RoundIcon } from '../components/cards/InfoCard'
-
+import InfoCard, { CTA, RoundIcon } from "../components/cards/InfoCard";
 
 function Cards() {
   return (
     <>
-      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
-     
-
-      <CTA />
-
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
+      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">
+        Cards
+      </h1>
 
       
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Elements
+      </h2>
 
-      <Card className="mb-8 shadow-md fullWidthCard">
-        <CardBody>
-          <p className="text-sm text-gray-600 dark:text-gray-400">
-            Large, full width sections goes here
-          </p>
-        </CardBody>
-      </Card>
+      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 microInteractionsContainer">
+        <Label className="nameInput">
+          <span>Name</span>
+          <Input className="mt-1" placeholder="Jane Doe" />
+        </Label>
+
+        <Label className="mt-4 nameInput">
+          <span>Disabled</span>
+          <Input disabled className="mt-1" placeholder="Jane Doe" />
+        </Label>
+
+        <div className="mt-4">
+          {/* TODO: Check if this label is accessible, or fallback */}
+          {/* <span className="text-sm text-gray-700 dark:text-gray-400">Account Type</span> */}
+          <Label>Account Type</Label>
+          <div className="mt-2 radioGroup">
+            <Label radio>
+              <Input type="radio" value="personal" name="accountType" />
+              <span className="ml-2">Personal</span>
+            </Label>
+            <Label className="ml-6" radio>
+              <Input type="radio" value="business" name="accountType" />
+              <span className="ml-2">Business</span>
+            </Label>
+            <Label disabled className="ml-6" radio>
+              <Input
+                disabled
+                type="radio"
+                value="disabled"
+                name="accountType"
+              />
+              <span className="ml-2">Disabled</span>
+            </Label>
+          </div>
+        </div>
+
+        <Label className="mt-4 select">
+          <span>Requested Limit</span>
+          <Select className="mt-1">
+            <option>$1,000</option>
+            <option>$5,000</option>
+            <option>$10,000</option>
+            <option>$25,000</option>
+          </Select>
+        </Label>
+
+        <Label className="mt-4 multiselect">
+          <span>Multiselect</span>
+          <Select className="mt-1" multiple>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+            <option>Option 4</option>
+            <option>Option 5</option>
+          </Select>
+        </Label>
+
+        <Label className="mt-4 textarea">
+          <span>Message</span>
+          <Textarea
+            className="mt-1"
+            rows="3"
+            placeholder="Enter some long form content."
+          />
+        </Label>
+
+        <Label className="mt-6 checkbox" check>
+          <Input type="checkbox" />
+          <span className="ml-2">
+            I agree to the <span className="underline">privacy policy</span>
+          </span>
+        </Label>
+      </div>
+
       
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Responsive cards</h2>
-   
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Responsive cards
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         <InfoCard title="Total clients" value="6389">
@@ -66,17 +140,39 @@ function Cards() {
         </InfoCard>
       </div>
 
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Cards with title</h2>
+ 
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Big section cards
+      </h2>
+
+      <Card className="mb-8 shadow-md fullWidthCard">
+        <CardBody>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            Large, full width sections goes here
+          </p>
+        </CardBody>
+      </Card>
+
+
+      <CTA />
+
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Cards with title
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <Card className="cardWithHeading">
           <CardBody>
-            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Revenue</p>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+              Revenue
+            </p>
             <p className="text-gray-600 dark:text-gray-400">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
@@ -85,15 +181,16 @@ function Cards() {
           <CardBody>
             <p className="mb-4 font-semibold">Colored card</p>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
       </div>
     </>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
