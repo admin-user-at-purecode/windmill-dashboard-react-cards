b48c3547af3c56d0c323879431f3759374124c53
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-08 15:37:04 +0530

    la_20

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,21 +1,53 @@
-import React from 'react'
-import { Card, CardBody } from '@windmill/react-ui'
-import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from '../assets/icons'
+import React from "react";
+import { Card, CardBody, Label } from "@windmill/react-ui";
+import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from "../assets/icons";
 
-import InfoCard, { CTA, RoundIcon } from '../components/cards/InfoCard'
-
+import InfoCard, { CTA, RoundIcon } from "../components/cards/InfoCard";
 
 function Cards() {
   return (
     <>
-      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
-     
+      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">
+        Cards
+      </h1>
 
       <CTA />
 
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Buttons
+      </h2>
 
-      
+      <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800 inputWithButtonContainer">
+        <Label className="inputWithButton">
+          <span>Button left</span>
+          <div className="relative">
+            <input
+              className="block w-full pl-20 mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <button className="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-l-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
+              Click
+            </button>
+          </div>
+        </Label>
+
+        <Label className="mt-4 inputWithButton">
+          <span>Button right</span>
+          <div className="relative text-gray-500 focus-within:text-purple-600">
+            <input
+              className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+              placeholder="Jane Doe"
+            />
+            <button className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+              Click
+            </button>
+          </div>
+        </Label>
+      </div>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Big section cards
+      </h2>
 
       <Card className="mb-8 shadow-md fullWidthCard">
         <CardBody>
@@ -24,9 +56,12 @@ function Cards() {
           </p>
         </CardBody>
       </Card>
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Responsive cards</h2>
-   
+
+ 
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Responsive cards
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         <InfoCard title="Total clients" value="6389">
@@ -66,17 +101,21 @@ function Cards() {
         </InfoCard>
       </div>
 
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Cards with title</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Cards with title
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <Card className="cardWithHeading">
           <CardBody>
-            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Revenue</p>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+              Revenue
+            </p>
             <p className="text-gray-600 dark:text-gray-400">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
@@ -85,15 +124,16 @@ function Cards() {
           <CardBody>
             <p className="mb-4 font-semibold">Colored card</p>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
       </div>
     </>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
