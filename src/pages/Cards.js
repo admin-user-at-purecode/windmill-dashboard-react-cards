2a07141c5767be09c955d6778514fc0d0e45e985
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-12 15:34:28 +0530

    la_32

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import { Card, CardBody } from '@windmill/react-ui'
-import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from '../assets/icons'
+import React from "react";
+import { Card, CardBody } from "@windmill/react-ui";
+import ChartCard, { ChartLegend } from "../components/chart/ChartCard";
+import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from "../assets/icons";
 
-import InfoCard, { CTA, RoundIcon } from '../components/cards/InfoCard'
+import InfoCard, { CTA, RoundIcon } from "../components/cards/InfoCard";
+import { Line } from "react-chartjs-2";
 
+import { lineOptions, lineLegends } from "../mock_data/chartData";
 
 function Cards() {
   return (
     <>
-      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
-     
+      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">
+        Cards
+      </h1>
 
       <CTA />
 
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
-
-      
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Big section cards
+      </h2>
 
       <Card className="mb-8 shadow-md fullWidthCard">
         <CardBody>
@@ -24,9 +28,20 @@ function Cards() {
           </p>
         </CardBody>
       </Card>
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Responsive cards</h2>
-   
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Chart
+      </h2>
+      <div className="grid mb-8 md:grid-cols-2">
+        <ChartCard title="Traffic">
+          <Line {...lineOptions} />
+          <ChartLegend legends={lineLegends} />
+        </ChartCard>
+      </div>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Responsive cards
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         <InfoCard title="Total clients" value="6389">
@@ -66,17 +81,21 @@ function Cards() {
         </InfoCard>
       </div>
 
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Cards with title</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Cards with title
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <Card className="cardWithHeading">
           <CardBody>
-            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Revenue</p>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+              Revenue
+            </p>
             <p className="text-gray-600 dark:text-gray-400">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
@@ -85,15 +104,16 @@ function Cards() {
           <CardBody>
             <p className="mb-4 font-semibold">Colored card</p>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
       </div>
     </>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
