eb092d7aed999a1f935936d771d473c761a7c1e9
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-12 12:21:11 +0530

    la_24

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,21 +1,110 @@
-import React from 'react'
-import { Card, CardBody } from '@windmill/react-ui'
-import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from '../assets/icons'
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardBody,
+  TableBody,
+  TableContainer,
+  Table,
+  TableHeader,
+  TableCell,
+  TableRow,
+  TableFooter,
+  Avatar,
+  Badge,
+  Pagination,
+} from "@windmill/react-ui";
+import { CartIcon, ChatIcon, MoneyIcon, PeopleIcon } from "../assets/icons";
 
-import InfoCard, { CTA, RoundIcon } from '../components/cards/InfoCard'
-
+import InfoCard, { CTA, RoundIcon } from "../components/cards/InfoCard";
+import response from "../mock_data/tableData";
 
 function Cards() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+
+  // pagination setup
+  const resultsPerPage = 10;
+  const totalResults = response.length;
+
+  // pagination change control
+  function onPageChange(p) {
+    setPage(p);
+  }
+
+  // on page change, load new sliced data
+  // here you would make another server request for new data
+  useEffect(() => {
+    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage));
+  }, [page]);
+
   return (
     <>
-      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">Cards</h1>
-     
+      <h1 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200 pageTitle">
+        Cards
+      </h1>
 
       <CTA />
 
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Big section cards</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Table
+      </h2>
 
-      
+      <TableContainer>
+        <Table>
+          <TableHeader>
+            <tr>
+              <TableCell>Client</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Date</TableCell>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            {data.map((user, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <div className="flex items-center text-sm">
+                    <Avatar
+                      className="hidden mr-3 md:block"
+                      src={user.avatar}
+                      alt="User image"
+                    />
+                    <div>
+                      <p className="font-semibold">{user.name}</p>
+                      <p className="text-xs text-gray-600 dark:text-gray-400">
+                        {user.job}
+                      </p>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">$ {user.amount}</span>
+                </TableCell>
+                <TableCell>
+                  <Badge type={user.status}>{user.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">
+                    {new Date(user.date).toLocaleDateString()}
+                  </span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TableFooter>
+          <Pagination
+            totalResults={totalResults}
+            resultsPerPage={resultsPerPage}
+            label="Table navigation"
+            onChange={onPageChange}
+          />
+        </TableFooter>
+      </TableContainer>
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Big section cards
+      </h2>
 
       <Card className="mb-8 shadow-md fullWidthCard">
         <CardBody>
@@ -24,9 +113,10 @@ function Cards() {
           </p>
         </CardBody>
       </Card>
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Responsive cards</h2>
-   
+
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Responsive cards
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         <InfoCard title="Total clients" value="6389">
@@ -66,17 +156,21 @@ function Cards() {
         </InfoCard>
       </div>
 
-      
-      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Cards with title</h2>
+      <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+        Cards with title
+      </h2>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <Card className="cardWithHeading">
           <CardBody>
-            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">Revenue</p>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+              Revenue
+            </p>
             <p className="text-gray-600 dark:text-gray-400">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
@@ -85,15 +179,16 @@ function Cards() {
           <CardBody>
             <p className="mb-4 font-semibold">Colored card</p>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, cum commodi a omnis
-              numquam quod? Totam exercitationem quos hic ipsam at qui cum numquam, sed amet
-              ratione! Ratione, nihil dolorum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
+              cum commodi a omnis numquam quod? Totam exercitationem quos hic
+              ipsam at qui cum numquam, sed amet ratione! Ratione, nihil
+              dolorum.
             </p>
           </CardBody>
         </Card>
       </div>
     </>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
